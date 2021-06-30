8c952b7e0e6365c9d848464c8476f31b65dcedad
Iryna Furmaniuk <furmaniukira@gmail.com>
2021-06-30 14:56:29 +0300

    Revert "Revert "set secretValue""
    This reverts commit 705d15e9864a57d4e6b285091e56d516626f90b3.

--- Git_practice_and_game_v2_with_test/src/com/company/Model.java
+++ Git_practice_and_game_v2_with_test/src/com/company/Model.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Model {
+    private int minBarrier;
+    private int maxBarrier;
+
+    private int secretValue;
+
+    // [1-99]
+    public void setSecretValue(){
+        secretValue = (int)Math.ceil(Math.random()*
+                (maxBarrier - minBarrier - 1) + minBarrier);
+    }
 }
